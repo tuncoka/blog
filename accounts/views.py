908be96a68bf6c6490461853fbc679fef94074b7
Ahmet Tunçok <ahmttnck@gmail.com>
2018-11-14 09:52:06 +0300

    initial commit

---
+++ accounts/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render, redirect
+from accounts.forms import LoginForm, RegisterForm
+from django.contrib.auth import authenticate, login, logout
+
+
+def login_view(request):
+    form = LoginForm(request.POST or None)
+    if form.is_valid():
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
+        user = authenticate(username=username, password=password)
+        login(request, user)
+        return redirect('home')
+    context = {
+        'form': form,
+        'title': 'Giriş Yap'
+    }
+    return render(request, 'accounts/form.html', context)
+
+
+def register_view(request):
+    form = RegisterForm(request.POST or None)
+    if form.is_valid():
+        user = form.save(commit=False)
+        password = form.cleaned_data.get('password1')
+        user.set_password(password)
+        # user.is_staff = user.is_superuser = True
+        user.save()
+        new_user = authenticate(username=user.username, password=password)
+        login(request, new_user)
+        return redirect('home')
+    context = {
+        'form': form,
+        'title': 'Üye Ol'
+    }
+    return render(request, 'accounts/form.html', context)
+
+def logout_view(request):
+    logout(request)
+    return redirect('home')
